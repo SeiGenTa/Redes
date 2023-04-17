f508065c85bbded32320ddffb9bc183095a857ae
AlMartinez01 <almartinez@ug.uchile.cl>
2023-04-17 01:06:00 -0400

    se subio la materia de semana 3-4 y se esta realizando la actividad

---
+++ Material/Semana3-4/server_proxy1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# proxy 
+# Mono-cliente, versión simple con select
+import select
+import sys
+import jsockets
+
+# Este el servidor de un socket ya conectado
+# y el cliente del verdadero servidor (host, portout)
+def proxy(conn, host, portout):
+
+    conn2 = jsockets.socket_tcp_connect(host, portout)
+    if conn2 is None:
+        print('conexión rechazada por '+host+', '+portout)
+        sys.exit(1)
+
+    inputs = [conn, conn2]
+
+    while inputs:
+        readable,writable,exceptional = select.select(inputs,[],inputs)
+        for s in exceptional: # cerramos sockets con error
+            print('Cliente con error')
+            conn.close()
+            conn2.close()
+            return
+        for s in readable: # sockets con datos para mi
+            if s is conn:
+                data = conn.recv(1024*1024)
+                if not data: # EOF
+                    conn.close()
+                    conn2.close()
+                    return
+                conn2.send(data)
+            if s is conn2:
+                data = conn2.recv(1024*1024)
+                if not data: # EOF
+                    conn.close()
+                    conn2.close()
+                    return
+                conn.send(data)
+
+# Main:
+
+if len(sys.argv) != 4:
+    print('Use: '+sys.argv[0]+' port-in host port-out')
+    sys.exit(1)
+
+portin = sys.argv[1]
+host = sys.argv[2]
+portout = sys.argv[3]
+
+s = jsockets.socket_tcp_bind(portin)
+if s is None:
+    print('bind falló')
+    sys.exit(1)
+
+
+while True:
+    conn, addr = s.accept();
+    print(f'Cliente conectado: {addr}')
+    proxy(conn, host, portout)
+    print('Cliente desconectado')
---
+++ Material/Semana3-4/server_proxy2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+# proxy 
+# Usando procesos para multi-clientes y threads dentros de cada proxy
+import os, signal
+import sys
+import jsockets
+import threading
+
+def childdeath(signum, frame):
+    os.waitpid(-1, os.WNOHANG)
+
+def copy_sock(conn1, conn2):
+    while True:
+        try:
+            data = conn1.recv(1500)
+        except:
+            data = None
+        if not data: break
+        conn2.send(data)
+    conn2.close()
+    print('lost')
+
+# Este el servidor de un socket ya conectado
+# y el cliente del verdadero servidor (host, portout)
+def proxy(conn, host, portout):
+
+    conn2 = jsockets.socket_tcp_connect(host, portout)
+    if conn2 is None:
+        print('conexión rechazada por '+host+', '+portout)
+        sys.exit(1)
+
+    print('Cliente conectado')
+
+# copy_sock() corre en ambos threads, uno de conn->conn2 y otro de conn2->conn
+    newthread1 = threading.Thread(target=copy_sock, daemon=True, args=(conn,conn2)) # el flag daemon es para que muera si muere el otro 
+    newthread1.start()
+    copy_sock(conn2, conn)
+    print('Cliente desconectado')
+
+
+# Main    
+if len(sys.argv) != 4:
+    print('Use: '+sys.argv[0]+' port-in host port-out')
+    sys.exit(1)
+
+portin = sys.argv[1]
+host = sys.argv[2]
+portout = sys.argv[3]
+
+signal.signal(signal.SIGCHLD, childdeath)
+
+s = jsockets.socket_tcp_bind(portin)
+if s is None:
+    print('bind falló')
+    sys.exit(1)
+
+while True:
+    conn, addr = s.accept()
+    pid = os.fork()
+    if pid == 0: # Este es el hijo
+        s.close() # Cierro el socket que no voy a usar
+        proxy(conn, host, portout)
+        sys.exit(0)
+    else:
+        conn.close() # Cierro el socket que no voy a usar
