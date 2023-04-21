be93af3ec092c3db0f34c401a42d548d3cb135d5
AlMartinez01 <almartinez@ug.uchile.cl>
2023-04-21 19:16:14 -0400

    se termino el codigo de programacion la tarea 1

--- Material/Tarea_1/bwc.py
+++ Material/Tarea_1/bwc.py
@@ -1,6 +1,8 @@
 import jsockets
 import sys
 import time
+import pathlib
+import os
 
 if len(sys.argv) != 5:
     print('Use: '+sys.argv[0]+' size_file file host port')
@@ -19,12 +21,14 @@ size = sys.argv[1]
 myFile = sys.argv[2]
 
 def sendC(syze:int):
-    s.settimeout(5)
+    s.settimeout(10)
+    while len(syze) < 4:
+        syze = "0" + str(syze)
     for i in range(5):
         time_init = time.time()
         s.send((f"C{syze}").encode())
         try:
-            data = s.recv(12).decode()
+            data = s.recv(1024).decode()
             if (data[0] != "C") or (not data[1:].isdigit()):
                 print("Respuesta no esperada")
                 return 0 #Respuesta no esperada
@@ -37,10 +41,10 @@ def sendC(syze:int):
         
     
 def sendE(syze):
-    s.settimeout(5)
+    s.settimeout(10)
     for i in range(5):
         time_init = time.time()
-        s.send((f"C{syze}").encode())
+        s.send((f"E").encode())
         try:
             data = s.recv(12).decode()
             if (data[0] != "E") or (not data[1:].isdigit()):
@@ -53,18 +57,33 @@ def sendE(syze):
     print("no hubo respuesta")
     return 0 #El 0 indicara que el server no respondio a nuestra peticion
 
-def sendD(new_size):
-    mysFile = open(myFile,"rb")
-    info = mysFile.read()
-    length_inf = info.__len__()
-    mul = 0
-    new_size -=1 ##cosiderando que D quita un byte
-    while(new_size * (mul+1) < length_inf):
-        s.send(f"D{(info[ new_size * mul : new_size * (mul + 1) - 1])}".encode())
-        mul += 1
-    s.send(info[ new_size * mul :])
-    mysFile.close()
-    return length_inf ##Retornamos el largo de la informacion
+def sendD(new_size,direccion = myFile,raiz = "./"):
+    length_inf_total = 0
+    ruta_archivo = os.path.join(raiz, direccion)
+    if os.path.isfile(ruta_archivo):# Entrara en este if si la direccion es un archivo como una imagen
+        # Si el archivo es un archivo, enviar su contenido a través del socket
+        
+        with open(ruta_archivo, "rb") as f:
+            info = f.read()
+            length_inf = info.__len__()
+            length_inf_total += length_inf
+            paquetes = []
+            for i in range(0, len(info), (new_size-1)):
+                paquete = bytearray("D","utf-8") + info[i:i+(new_size-1)]
+                paquetes.append(paquete)
+                
+
+            for paquete in paquetes:
+                # Agregamos la letra "D" al inicio del paquete
+                # Aquí podríamos encriptar el paquete antes de enviarlo
+                s.send(paquete)
+        return length_inf_total
+    
+    #que llegue aqui significa que es una carpeta
+    mysFile = os.listdir(ruta_archivo)
+    for archivo in mysFile:
+        length_inf_total += sendD(new_size,direccion=archivo,raiz=ruta_archivo)
+    return length_inf_total
 
 def main():
     print(f"se propuso un tamaño de: {size}")
@@ -73,13 +92,23 @@ def main():
         print("el servidor respondio de maneria no esperada o simplemente no respondio")
         sys.exit(1)
     print(f"El servidor respondio con: {respC.decode()}")
-    
     cantBytes = int(respC)
+    
+    #Aqui iniciamos a enviar la informacion asi que aqui iniciaremos el cronometro
+    time_init = time.time()
     
     byteT = sendD(cantBytes) #Ya se sabemos sendD nos retorna la cantidad de bytes que se enviaron
     
+    #en este punto ya se enviaron todos los envios y esperaremos a que el servidor retorne su respuesta a que termino 
+    #el envio de archivos (envio E)
     cantReciv = sendE(cantBytes)
-    print(f"se envio: {byteT}, y se recibio: {cantReciv.decode()}")
+    if cantReciv == 0:
+        print("hubo en problema al recibir las respuestas")
+        sys.exit(0)
+    time_end = time.time()
+    
+    print(f"se envio: {byteT}, y se recibio: {cantReciv.decode()} ")
+    print(f"tardo: {time_end-time_init} segundos")
     
     
 main()
